4a7370e549efe0a54a376b052800db5de93db2a1
scespinoza <secancinoe@gmail.com>
2020-06-22 17:32:02 -0400

    fix walk graph

--- multimodal_network.py
+++ multimodal_network.py
@@ -38,7 +38,7 @@ def get_nearest(src_points, candidates, k_neighbors=1):
     return (closest, closest_dist)
 class MultiModalNetwork:
 
-    def __init__(self, layers, speeds):
+    def __init__(self, layers, speeds, directed):
         
         assert set(layers.keys()) == set(speeds.keys())
         self.layers = layers
@@ -93,7 +93,7 @@ class MultiModalNetwork:
         self.g = {}
         for layer_name, layer_gdfs in self.layers.items():
             print('Creating {} graph...'.format(layer_name))
-            layer_graph = igraph.Graph(directed=True)
+            layer_graph = igraph.Graph(directed=self.directed[layer_name])
             layer_graph.add_vertices(list(layer_gdfs['nodes'].osmid.astype(str)))
             layer_graph.vs['coords'] = list(layer_gdfs['nodes']['geometry'])
             layer_graph.vs['osmid'] = list(layer_gdfs['nodes']['osmid'].astype(str))
@@ -134,7 +134,7 @@ class MultiModalNetwork:
         transfer_nodes = self.layers['bike']['nodes'][self.layers['bike']['nodes']['osmid'].apply(lambda osmid: osmid in self.transfer_nodes)]
         transfer_nodes.plot(ax=ax, c='yellow', alpha=0.3, markersize=5)
     @classmethod
-    def from_polygon(cls, polygon, layers=['walk', 'bike'], save=True, speeds={'walk': 1., 'bike': 1.}):
+    def from_polygon(cls, polygon, layers=['walk', 'bike'], save=True, speeds={'walk': 1., 'bike': 1.}, directed={'walk':False, 'bike': 'True'}):
         layers_graph = {}
         for layer in layers:
             
@@ -155,14 +155,14 @@ class MultiModalNetwork:
             
             layers_graph[layer] = {'nodes': nodes, 'edges': edges}
 
-        return cls(layers_graph, speeds=speeds)
+        return cls(layers_graph, speeds=speeds, directed=directed)
 
 
 if __name__ == '__main__':
     study_area_filename = 'shapes/study_area/study_area.shp'
     study_area = gpd.read_file(study_area_filename).to_crs('EPSG:4326')
     study_area_polygon = study_area.iloc[0]['geometry']
-    network = MultiModalNetwork.from_polygon(study_area_polygon, speeds={'walk': 1.4, 'bike':2.16})
+    network = MultiModalNetwork.from_polygon(study_area_polygon, speeds={'walk': 1.4, 'bike':2.16}, directed={'walk': False, 'bike': True})
     fig, ax = plt.subplots(figsize=(13, 13))
     network.plot(ax=ax)
     plt.show()
