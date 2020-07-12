d7ea835b6390bfa4b6989f2c2105b352fef25a9a
scespinoza <secancinoe@gmail.com>
2020-07-12 17:50:23 -0400

    add variable batch size

--- models_torch.py
+++ models_torch.py
@@ -55,7 +55,7 @@ class ActorNetwork(nn.Module):
         return torch.stack(a).view(-1, t, nzones)
 
 class SimpleSubActor(nn.Module):
-    def __init__(self, neurons=64, input_size=16):
+    def __init__(self, neurons=128, input_size=16):
         super(SimpleSubActor, self).__init__()
         self.bn1 = nn.LayerNorm(input_size)
         self.fc1 = nn.Linear(input_size,neurons)
@@ -70,7 +70,7 @@ class SimpleSubActor(nn.Module):
         return x
 
 class SimpleActor(nn.Module):
-    def __init__(self, state_size=6, gru_out=16, nzones=100):
+    def __init__(self, state_size=6, gru_out=32, nzones=100):
         super(SimpleActor, self).__init__()
         self.nzones = nzones
         self.gru = nn.GRU(state_size, gru_out, batch_first=True)
@@ -86,7 +86,7 @@ class SimpleActor(nn.Module):
         return torch.stack(a).view(-1, t, nzones)
     
 class LocalizedModule(nn.Module):
-    def __init__(self, neurons=128, state_size=6):
+    def __init__(self, neurons=256, state_size=6):
         super(LocalizedModule, self).__init__()
         # Input is state_size * neighbors + price (action)
         self.ln1 = nn.LayerNorm((state_size + 1) * 5)
@@ -153,12 +153,12 @@ class CriticNetwork(nn.Module):
         return torch.sum(f + q, dim=1)
 
 class SimpleSubCritic(nn.Module):
-    def __init__(self, input_size=16):
+    def __init__(self, neurons=128, input_size=16):
         super(SimpleSubCritic, self).__init__()
         self.bn1 = nn.LayerNorm(input_size)
-        self.fc1 = nn.Linear(input_size, 16)
-        self.bn2 = nn.LayerNorm(16)
-        self.fc2 = nn.Linear(16, 1)
+        self.fc1 = nn.Linear(input_size, neurons)
+        self.bn2 = nn.LayerNorm(neurons)
+        self.fc2 = nn.Linear(neurons, 1)
         self.apply(init_uniform)
 
     def forward(self, x):
@@ -169,7 +169,7 @@ class SimpleSubCritic(nn.Module):
         return x[:, -1, :]
 
 class SimpleCritic(nn.Module):
-    def __init__(self, gru_out=16, state_size=6, nzones=100):
+    def __init__(self, gru_out=32, state_size=6, nzones=100):
         super(SimpleCritic, self).__init__()
         self.nzones = nzones
         self.gru = nn.GRU((state_size + 1) * 5, gru_out, batch_first=True)
