7c780c42d71339fa6c757b77b4300951d4eb3629
scespinoza <secancinoe@gmail.com>
2020-07-12 21:21:00 -0400

    add variable batch size

--- models_torch.py
+++ models_torch.py
@@ -154,7 +154,7 @@ class SimpleSubCritic(nn.Module):
 
     def forward(self, x):
         x = nn.ReLU()(self.fc1(x))
-        x = nn.ReLU()(self.fc2(x))
+        x = self.fc2(x)
         return x[:, -1, :]
 
 class SimpleCritic(nn.Module):
