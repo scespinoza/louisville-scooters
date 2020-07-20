d874e3d314d55957b1ae4c35a0a41ccdc748ca0f
scespinoza <secancinoe@gmail.com>
2020-07-20 16:38:23 -0400

    clip gradient

--- models_torch.py
+++ models_torch.py
@@ -243,6 +243,7 @@ class HRP(nn.Module):
         grad = self.actor_gradient(batch)
         self.actor_optimizer.zero_grad()
         grad.backward()
+        torch.nn.utils.clip_grad_norm_(an.parameters(), 1.0)
         self.actor_optimizer.step()
         return grad.detach().cpu().numpy()
 
