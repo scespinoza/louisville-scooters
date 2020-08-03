9eb83053e8905e58c66ec46768f5286fe5add37c
scespinoza <secancinoe@gmail.com>
2020-08-02 21:23:28 -0400

    update cost

--- simulation.py
+++ simulation.py
@@ -280,7 +280,7 @@ class User:
         self.max_walking_distance = 500 # km
 
         self.velocity = 1.5 # m/s
-        self.alpha = 1.888e-6
+        self.alpha = 5.875e-07
         self.trip = {
                 'id': self.user_id,
                 'origin': self.origin,
